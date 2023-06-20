90ced236bcfc6746c178da6cf4223c9e5afbdf46
Rory Caston <rory@caston.co.uk>
2023-06-20 03:49:45 +0100

    successful test linking to test sheet

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import os
 
 from bot.bot_setup import client
 
+from sheets.test import *
+
 
 def run():
 
-    TOKEN = os.getenv('SIGNS_TOKEN')
-    client.run(TOKEN)
+    """TOKEN = os.getenv('SIGNS_TOKEN')
+    client.run(TOKEN)"""
+
+    main()
 
 
 if __name__ == '__main__':
